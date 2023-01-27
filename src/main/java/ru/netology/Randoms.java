dca31263b7493aa7948b5c2c80ca1bfcdd49027c
Nina Galagan <kler.mongomeri.9851@mail.ru>
2023-01-27 21:13:45 +0300

    Добавление папок

---
+++ src/main/java/ru/netology/Randoms.java
@@ -0,0 +1,31 @@
+package ru.netology;
+import java.util.Iterator;
+import java.util.PrimitiveIterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected PrimitiveIterator.OfInt random;
+    protected int min;
+    protected int max;
+
+    public Randoms(int min, int max) {
+        this.min=min;
+        this.max=max;
+        random=new Random().ints(min,(max+1)).iterator();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext(){
+                return true;
+            }
+
+            @Override
+            public Integer next(){
+                return random.nextInt();
+            }
+        };
+    }
+}
